914196923aed4a2a60bbd29af833c127bea47b65
Luke-Dramko <luke.dramko@gmail.com>
2018-12-15 20:49:13 -0600

    Added error file for swift enums that represent runtime errors/exceptions.  Added an enumeration for when approximation fails due to an undefined constant.

---
+++ Errors.swift
@@ -0,0 +1,13 @@
+//
+//  Errors.swift
+//  NumbersDevelop
+//
+//  Created by Luke  Dramko on 12/15/18.
+//  Copyright © 2018 Luke Dramko. All rights reserved.
+//
+
+import Foundation
+
+enum ApproximationError: Error {
+    case ConstantDoesNotHaveApproximateValueError(String)
+}
