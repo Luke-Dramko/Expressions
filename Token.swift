7de9c82888ad17e3b47862b71485f4203adee92f
Luke-Dramko <luke.dramko@gmail.com>
2019-01-11 14:42:37 -0600

    Framework for Token enum to represent token types and associated information.

---
+++ Token.swift
@@ -0,0 +1,18 @@
+//
+//  Token.swift
+//  NumbersDevelop
+//
+//  Created by Luke  Dramko on 1/11/19.
+//  Copyright © 2019 Luke Dramko. All rights reserved.
+//
+
+import Foundation
+
+//Represents a token from a string representing a mathematical expression.
+internal enum Token {
+    case addition
+    case division
+    case subtraction
+    case multiplication
+    //more to come
+}
