f28913cdcf70a99337b20ff8dfffb66ff9cb9c6f
Luke-Dramko <luke.dramko@gmail.com>
2019-01-11 20:43:52 -0600

    Refactored ExpressionTokenizer structure.

--- ExpressionTokenizer.swift
+++ ExpressionTokenizer.swift
@@ -9,24 +9,28 @@
 
 import Foundation
 
-struct ExpressionTokenizer: Sequence {
+struct ExpressionTokenizer {
     var expression: String
+    var current: Token = .empty
     
-    init(_ e: String) {
-        self.expression = e;
-    }
-    
-    internal func makeIterator() -> ExpressionTokenizer.Iterator {
-        <#code#>
-    }
-    
-    /**
-     Iterator returns the next token, or none if there's nothing left in the string to parse.
-     */
-    struct Iterator: IteratorProtocol {
-        
-        mutating func next() -> Token? {
-            
+    init(_ e: String) throws {
+        if e.count == 0 { //empty string can't be parsed and doesn't make sense as an expression.
+            throw ParseError.EmptyExpression
         }
+        self.expression = e;
+        self.current = next();
     }
+    
+    private func next() -> Token {
+        return .empty //temporary placeholder
+    }
+    
+    internal func peek() -> Token {
+        return .empty //temporary placeholder
+    }
+    
+    internal func pop() -> Token {
+        return .empty //temporary placeholder
+    }  
+    
 }
