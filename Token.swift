b2dd4e2dbcf0d24fc94d9983c1fa761247669eaa
Luke-Dramko <luke.dramko@gmail.com>
2019-01-12 21:18:02 -0600

    Added lparen and rparen token types in Token enum.

--- Token.swift
+++ Token.swift
@@ -17,6 +17,8 @@ internal enum Token {
     case power //matches ^
     case symbol(String) //matches an symbolic constant: a, e, \pi, \lambda, etc.
     case integer(Int) //Matches an integer.  4, -2, 7, etc.
+    case lparen
+    case rparen
     case error(remainder: String) //matches an invalid piece of input.
     case empty //describes an empty string.
 }
