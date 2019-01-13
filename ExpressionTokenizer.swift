df3a6777a416fcea89622753b56ef187dc2a64fe
Luke-Dramko <luke.dramko@gmail.com>
2019-01-12 21:17:38 -0600

    Modified regexes, added convenience extension to NSRegularExpression, finished next() function in ExpressionTokenizer.

--- ExpressionTokenizer.swift
+++ ExpressionTokenizer.swift
@@ -11,16 +11,19 @@ import Foundation
 
 struct ExpressionTokenizer {
     var expression: String
-    var current: Token = .empty
+    var current: Token = .empty //placeholder value
     
     //Regular expressions
     let add_sign = try! NSRegularExpression(pattern: "^\\+")
     let minus_sign = try! NSRegularExpression(pattern: "^-")
     let multiplication_sign = try! NSRegularExpression(pattern: "^\\*")
     let division_sign = try! NSRegularExpression(pattern: "^/")
+    let exponentiation_sign = try! NSRegularExpression(pattern: "^^")
     let symbol = try! NSRegularExpression(pattern: "^[a-zA-Z]|\\\\[a-zA-Z]+")
     let integer = try! NSRegularExpression(pattern: "^[0-9]+")
-    let whitespace = try! NSRegularExpression(pattern: "^\\s")
+    let whitespace = try! NSRegularExpression(pattern: "^\\s*")
+    let lparen = try! NSRegularExpression(pattern: "^\\(")
+    let rparen = try! NSRegularExpression(pattern: "^\\)")
     
     init(_ e: String) throws {
         if e.count == 0 { //empty string doesn't make sense as an expression.
@@ -31,7 +34,31 @@ struct ExpressionTokenizer {
     }
     
     private func next() -> Token {
-        return .empty //temporary placeholder
+        if let _ = add_sign.firstMatch(in: expression) {
+            return .addition
+        } else if let _ = minus_sign.firstMatch(in: expression) {
+            return .subtraction
+        } else if let _ = division_sign.firstMatch(in: expression) {
+            return .division
+        } else if let _ = multiplication_sign.firstMatch(in: expression) {
+            return .multiplication
+        } else if let _ = exponentiation_sign.firstMatch(in: expression) {
+            return .power
+        } else if let s = symbol.firstMatch(in: expression) {
+            return .symbol(s)
+        } else if let i = integer.firstMatch(in: expression) {
+            return .integer(Int(i)!) //force-unwrap is okay because regular expression ensures it's an int.
+        } else if let _ = whitespace.firstMatch(in: expression) {
+            return self.next()
+        } else if let _ = lparen.firstMatch(in: expression) {
+            return .lparen
+        } else if let _ = rparen.firstMatch(in: expression) {
+            return .rparen
+        } else if expression == "" {
+            return .empty
+        }
+        
+        return .error(remainder: expression)
     }
     
     internal func peek() -> Token {
@@ -42,4 +69,17 @@ struct ExpressionTokenizer {
         return .empty //temporary placeholder
     }  
     
+}
+
+extension NSRegularExpression {
+    public func firstMatch(in str: String) -> String? {
+        //self.firstMatch(in: str, options: range: NSRange(str.startIndex..., in: str)))
+        let match = self.firstMatch(in: str, range: NSRange(str.startIndex..., in: str))
+        
+        return match.map {
+            String(str[Range($0.range, in: str)!])
+        }
+        
+        
+    }
 }
--- main.swift
+++ main.swift
@@ -119,6 +119,6 @@ print("before sorting, array = \(array)")
 array.sort()
 print("after sorting, array = \(array)") */
 
-let tokenizer = try! ExpressionTokenizer("abc")
-print(tokenizer.add_sign.firstMatch(in: "+abc", range: NSMakeRange(0, "+abc".count)))
+let tokenizer = try! ExpressionTokenizer("+abc")
+//print(tokenizer.next())
 
