925638d6df8f5c65973dd1eab8133b4f4c397759
Luke-Dramko <luke.dramko@gmail.com>
2019-01-30 20:57:13 -0600

    Added global header to version control.

---
+++ Expressions/Expressions.h
@@ -0,0 +1,19 @@
+//
+//  Expressions.h
+//  Expressions
+//
+//  Created by Luke  Dramko on 1/30/19.
+//  Copyright © 2019 Luke Dramko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Expressions.
+FOUNDATION_EXPORT double ExpressionsVersionNumber;
+
+//! Project version string for Expressions.
+FOUNDATION_EXPORT const unsigned char ExpressionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Expressions/PublicHeader.h>
+
+
